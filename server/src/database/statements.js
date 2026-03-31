1088da4a1f3f4d9f124f68855750cab5759d8d5d
mirandashor <mirandashoraji@oakland.edu>
2026-03-30 21:40:50 -0400

    add statements.js

---
+++ server/src/database/statements.js
@@ -0,0 +1,78 @@
+const { clear } = require('node:console');
+
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+const dbPath = path.join(__dirname, "database.db");
+const db = new sqlite3.Database(dbPath);
+
+db.serialize(() => { //run in order
+    //user table - user details from spotify
+    // user id (autoincrements so each user has a new id for table
+    // spotify id, spotify display name
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users 
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        spotify_user_id TEXT,
+        display_name TEXT        
+        )
+        `);
+    //top tracks table - users top tracks from spotify API
+    // track id for table (autoincremenets)
+    // user id = id for each user in user table
+    // spotify track id, track name, artist name, rank (placement)
+    db.run(`
+        CREATE TABLE IF NOT EXISTS top_tracks
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        spotify_track_id TEXT,
+        track_name  TEXT,
+        artist_name TEXT,
+        rank INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users(id) 
+        )
+        `);
+});
+//insert the user and their top tracks into the database dynamically
+function saveUserAndTracks(user, tracks) {
+    db.run(
+        //insert the info using their spotify ID and display name
+        `INSERT INTO users (spotify_user_id, display_name)
+        VALUES (?, ?)`,
+        [user.id, user.display_name],
+        function(err) {
+            if (err) return console.error(err);
+
+            let userId = this.lastID
+
+            insertTracks(userId, tracks);
+            }
+    );
+}
+
+//insert a users top tracks into a database using the user ID
+//from the user table to get their spotify data
+function insertTracks(userId, tracks) {
+    tracks.forEach((track, index) => {
+        db.run(
+            `INSERT INTO top_tracks
+            (user_id, spotify_track_id, track_name, artist_name, rank)
+            VALUES (?, ?, ?, ?, ?)`,
+            [userId, track.id, track.name, track.artist, index]
+        );
+    });
+    console.log("Top Tracks Saved!")
+}
+//delete the data in the database after a session ends
+function clearSessionData() {
+    db.run(`DELETE FROM top_tracks`);
+    db.run(`DELETE FROM users`);
+    console.log("Session data cleared");
+}
+
+module.exports = {
+    saveUserAndTracks,
+    clearSessionData
+};
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import { saveUserAndTracks } from "../database/statements";
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET!;
 const REDIRECT_URI = "http://127.0.0.1:5000/auth/callback";
 
 router.get("/login", (req, res) => {
-  const scope = "user-top-read playlist-modify-private";
+  const scope = "user-top-read playlist-modify-private user-read-private";
 
   const authURL =
     "https://accounts.spotify.com/authorize?" +
@@ -43,10 +44,43 @@ router.get("/callback", async (req, res) => {
     );
 
     const access_token = tokenResponse.data.access_token;
+    
+    // get user info (user ID and display name - no private info)
+    const spotifyUser = await axios.get(
+      "https://api.spotify.com/v1/me",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    const user = {
+      id: spotifyUser.data.id,
+      display_name: spotifyUser.data.display_name
+    };
 
-    console.log("ACCESS TOKEN:", access_token);
+    // get users top tracks
+    const topTracks = await axios.get(
+      "https://api.spotify.com/v1/me/top/tracks",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
 
-    res.redirect(`http://localhost:5173/?access_token=${access_token}`);
+    const tracks = topTracks.data.items.map((track: any) => ({
+      id: track.id,
+      name: track.name,
+      artist: track.artists[0].name
+    }));
+
+    saveUserAndTracks(user, tracks);
+
+// redirect to frontend with data
+res.redirect(
+  `http://localhost:5173/?tracks=${encodeURIComponent(JSON.stringify(tracks))}`
+);
 
   } catch (error: any) {
     console.error(error.response?.data || error.message);
