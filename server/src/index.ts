9d4b15c8fb13160baa19e068ddac7e91e9a7edc9
mirandashor <mirandashoraji@oakland.edu>
2026-04-03 23:13:31 -0400

    add start script for render

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import playlistRoutes from "./routes/playlistRoutes";
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -25,5 +25,5 @@ app.use("/auth", authRoutes);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
